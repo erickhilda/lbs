c0057a78b8eef0e838274e69d606ea1a449988d0
erickhilda <erickh.andreas@gmail.com>
2025-12-18 00:11:08 +0700

    chore: Disable foreign key checks when clearing test DB and clear db after all test run

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,6 +4,8 @@ import { db, initializeDb } from "../src/database/db";
 initializeDb();
 
 function clearDatabase() {
+  db.run("PRAGMA foreign_keys = OFF");
+
   db.run("DELETE FROM borrow_records");
   db.run("DELETE FROM books");
   db.run("DELETE FROM members");
@@ -11,6 +13,8 @@ function clearDatabase() {
   db.run("DELETE FROM sqlite_sequence WHERE name='borrow_records'");
   db.run("DELETE FROM sqlite_sequence WHERE name='books'");
   db.run("DELETE FROM sqlite_sequence WHERE name='members'");
+
+  db.run("PRAGMA foreign_keys = ON");
 }
 
 beforeEach(() => {
@@ -18,5 +22,6 @@ beforeEach(() => {
 });
 
 afterAll(() => {
+  clearDatabase();
   db.close();
 });
